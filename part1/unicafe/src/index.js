fe7b4201f2030cd279aa6459e7f07354163b8e10
francoromanolosada <francoromano1994@gmail.com>
2021-02-27 20:23:52 +0100

    Added part1 - unicafe excercise.

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
+  const setToZero = () => setCounter(0);
+
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button handleClick={increaseByOne} text={'Plus'} />
+      <Button handleClick={decreaseByOne} text={'Rest'} />
+      <Button handleClick={setToZero} text={'Reset'} />
+    </div>
+  );
+};
+
+const Display = ({ counter }) => <div>{counter}</div>;
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
