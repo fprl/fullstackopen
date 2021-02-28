58fc478ad4178c2579eaa35deb7658b8908ca22e
francoromanolosada <francoromano1994@gmail.com>
2021-02-28 22:18:16 +0100

    d A more complex state, following excercise.

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,26 +2,47 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'));
+    setLeft(left + 1);
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'));
+    setRight(right + 1);
+  };
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button handleClick={increaseByOne} text={'Plus'} />
-      <Button handleClick={decreaseByOne} text={'Rest'} />
-      <Button handleClick={setToZero} text={'Reset'} />
+      {left}
+      <Button onClick={handleLeftClick} text={'left'} />
+      <Button onClick={handleRightClick} text={'right'} />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   );
 };
 
-const Display = ({ counter }) => <div>{counter}</div>;
+const History = ({allClicks}) => {
+  if (allClicks.length === 0) {
+    return <div>The app is used by clicking the buttons!</div>;
+  }
+  return (
+    <div>
+      button press history: {allClicks.join(' ')}
+    </div>
+  )
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>{text}</button>
-);
+};
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
 
 ReactDOM.render(<App />, document.getElementById('root'));
