756ce68cdbc1e3fe125e0ab5238224f949d91dbc
francoromanolosada <francoromano1994@gmail.com>
2021-03-01 17:10:12 +0100

    1.6: unicafe step1 exercise.

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,47 +2,47 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'));
-    setLeft(left + 1);
-  };
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'));
-    setRight(right + 1);
-  };
-
-  return (
-    <div>
-      {left}
-      <Button onClick={handleLeftClick} text={'left'} />
-      <Button onClick={handleRightClick} text={'right'} />
-      {right}
-      <History allClicks={allClicks} />
-    </div>
-  );
-};
-
-const History = ({allClicks}) => {
-  if (allClicks.length === 0) {
-    return <div>The app is used by clicking the buttons!</div>;
+  const handleClick = (state, feedback) => {
+    state(feedback + 1);  
   }
+  
   return (
     <div>
-      button press history: {allClicks.join(' ')}
+      <Title text={'give feedback'} />
+      <Button handleClick={() => handleClick(setGood, good)} text={'good'} />
+      <Button handleClick={() => handleClick(setNeutral, neutral)} text={'neutral'} />
+      <Button handleClick={() => handleClick(setBad, bad)} text={'bad'} />
+      <Title text={'statistics'} />
+      <Total text={'good'} feedback={good} />
+      <Total text={'neutral'} feedback={neutral} />
+      <Total text={'bad'} feedback={bad} />
     </div>
   )
-
 };
 
-const Button = ({ onClick, text }) => (
-  <button onClick={onClick}>
-    {text}
-  </button>
-)
+// Components
+const Title = ({text}) => {
+  return (
+    <>
+      <h1>{text}</h1>
+    </>
+  )
+}
+
+const Button = ({handleClick, text}) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
+const Total = ({text, feedback}) => {
+  return(
+    <p>{text} {feedback}</p>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
