ad4720ba60dee6573ea0e6c4290f0ed11d369f4e
francoromanolosada <francoromano1994@gmail.com>
2021-03-01 17:28:19 +0100

    1.7: unicafe step2 excercise.

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -5,44 +5,45 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0)
 
   const handleClick = (state, feedback) => {
-    state(feedback + 1);  
-  }
+    state(feedback + 1);
+    setAll(all + 1);
+  };
   
   return (
     <div>
       <Title text={'give feedback'} />
       <Button handleClick={() => handleClick(setGood, good)} text={'good'} />
-      <Button handleClick={() => handleClick(setNeutral, neutral)} text={'neutral'} />
+      <Button handleClick={() => handleClick(setNeutral, neutral)} text={'neutral'}/>
       <Button handleClick={() => handleClick(setBad, bad)} text={'bad'} />
       <Title text={'statistics'} />
-      <Total text={'good'} feedback={good} />
-      <Total text={'neutral'} feedback={neutral} />
-      <Total text={'bad'} feedback={bad} />
+      <Statistic text={'good'} feedback={good} />
+      <Statistic text={'neutral'} feedback={neutral} />
+      <Statistic text={'bad'} feedback={bad} />
+      <Statistic text={'all'} feedback={all} />
+      <Statistic text={'average'} feedback={(good - bad) / all} />
+      <Statistic text={'positive'} feedback={(good / all) * 100} />
     </div>
-  )
+  );
 };
 
 // Components
-const Title = ({text}) => {
-  return (
-    <>
-      <h1>{text}</h1>
-    </>
-  )
-}
+const Title = ({ text }) => {
+  return <><h1>{text}</h1></>
+};
 
-const Button = ({handleClick, text}) => {
-  return (
-    <button onClick={handleClick}>{text}</button>
-  )
-}
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
 
-const Total = ({text, feedback}) => {
-  return(
-    <p>{text} {feedback}</p>
-  )
-}
+const Statistic = ({ text, feedback }) => {
+  return (
+    <p>
+      {text} {feedback}
+    </p>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
