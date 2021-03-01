93f2017daced3e6761d576c7d9c34bb02626e466
francoromanolosada <francoromano1994@gmail.com>
2021-03-01 23:34:18 +0100

    1.14*: anecdotes step3 exercise.

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -2,36 +2,64 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = (props) => {
-  const [selected, setSelected] = useState(0)
-  const [votes, setVotes] = useState(props.votes)
-
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(props.votes);
+  console.log(selected)
+  console.log(votes)
   const handleClick = () => {
     const randomNumber = Math.floor(Math.random() * anecdotes.length);
     setSelected(randomNumber);
-  }
-  
+  };
+
   const handleVote = () => {
     const votesCopy = [...votes];
     votesCopy[selected] += 1;
-    setVotes(votesCopy)
-  }
+    setVotes(votesCopy);
+  };
 
-  console.log(votes)
   return (
     <div>
-      <h3>{props.anecdotes[selected]}</h3>
-      <h3>has {votes[selected]} votes</h3>
-      <Button handleClick={() => handleClick()} text={"next anecdote"} />
-      <Button handleClick={() => handleVote()} text={"vote"} />
+      <div>
+        <h1>Anecdote of the day</h1>
+        <Anecdote anecdotes={props.anecdotes} selected={selected} />
+        <Votes votes={votes} selected={selected} />
+        <Button handleClick={() => handleClick()} text={'next anecdote'} />
+        <Button handleClick={() => handleVote()} text={'vote'} />
+      </div>
+      <div>
+        <h1>Anecdote with most votes</h1>
+        <Anecdote anecdotes={props.anecdotes} votes={votes} />
+        <Votes votes={votes} />
+      </div>
     </div>
-  )
-}
+  );
+};
+
+const Anecdote = (props) => {
+  if (!props.votes) {
+    // anecdote of the day
+    return <p>{props.anecdotes[props.selected]}</p>;
+  }
+  // anecdote with most votes
+  const {votes} = props;
+  return (
+    <p>{props.anecdotes[votes.indexOf(Math.max(...votes))]}</p>
+  );
+};
+
+const Votes = (props) => {
+  // anecdote of the day votes
+  if (props.selected) {
+    return <p>has {props.votes[props.selected]} votes</p>;
+  }
+  // votes for the anecdote with most votes
+  const { votes } = props;
+  return <p>has {votes[votes.indexOf(Math.max(...votes))]} votes</p>;
+};
 
 const Button = (props) => {
-  return (
-    <button onClick={props.handleClick}>{props.text}</button>
-  )
-}
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
 
 const anecdotes = [
   'If it hurts, do it more often',
@@ -39,9 +67,15 @@ const anecdotes = [
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+];
 
-const votes = Array.apply(null, new Array(anecdotes.length)).map(Number.prototype.valueOf,0);
+const votes = Array.apply(null, new Array(anecdotes.length)).map(
+  Number.prototype.valueOf,
+  0
+);
 
-ReactDOM.render(<App anecdotes={anecdotes} votes={votes} />, document.getElementById('root'));
+ReactDOM.render(
+  <App anecdotes={anecdotes} votes={votes} />,
+  document.getElementById('root')
+);
