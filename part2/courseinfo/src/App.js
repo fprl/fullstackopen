e18f98d4ea24c8e5b9d3ba338a4588256dc54226
francoromanolosada <francoromano1994@gmail.com>
2021-03-02 23:55:35 +0100

    2.1: Course information step6 exercise.

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import Note from './components/Note';
+import Course from './components/Course';
 
-const App = ({ notes }) => {
-  return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map((note) => (
-          <Note key={note.id} note={note} />
-        ))}
-      </ul>
-    </div>
-  );
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3,
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
 };
 
 export default App;
