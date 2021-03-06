0b1c01b3c4d3d7d0b5fd41e0b98d9a474440d97c
francoromanolosada <francoromano1994@gmail.com>
2021-03-06 20:41:10 +0100

    2.18*: Phonebook step10 exercise.

--- part2/phonebook/src/components/PersonForm.js
+++ part2/phonebook/src/components/PersonForm.js
@@ -40,10 +40,10 @@ const PersonForm = ({persons, setNewRequest}) => {
   return (
     <form onSubmit={addPerson}>
     <div>
-      name: <input value={newName} onChange={e => setNewName(e.target.value)} />
+      name: <input type="text" value={newName} onChange={e => setNewName(e.target.value)} />
     </div>
     <div>
-      number: <input value={newPhone} onChange={e => setNewPhone(e.target.value)} />
+      number: <input type="tel" value={newPhone} onChange={e => setNewPhone(e.target.value)} />
     </div>
     <button type="submit">add</button>
   </form>
