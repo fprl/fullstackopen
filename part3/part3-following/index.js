bd63821eab9d3b115b0231b39e6625013937e30c
francoromanolosada <francoromano1994@gmail.com>
2021-03-09 12:29:11 +0100

    Following "b Deploying app to internet".

--- part3/part3-following/index.js
+++ part3/part3-following/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 
+app.use(express.static('build'))
+app.use(cors())
 app.use(express.json())
 
 const requestLogger = (request, response, next) => {
@@ -94,7 +97,7 @@ app.delete('/api/notes/:id', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
