88c89030d83a2e45206e8ccc61d977c045155a86
francoromanolosada <francoromano1994@gmail.com>
2021-03-11 10:40:20 +0100

    Following "c Saving data to MongoDB".

--- part3/part3-following/index.js
+++ part3/part3-following/index.js
@@ -43,14 +43,6 @@ let notes = [
   }
 ]
 
-const generateId = () => {
-  const maxId = 
-    notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-  return maxId + 1;
-}
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
@@ -62,34 +54,26 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  Note.findById(request.params.id)
+    .then(note => response.json(note))
 })
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId()
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
--- part3/part3-following/mongo.js
+++ part3/part3-following/mongo.js
@@ -1,38 +1,56 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
+const url = process.env.MONGODB_URI;
 
-if (process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
-  process.exit(1)
-}
-
-const password = process.argv[2];
-const dbName = 'note-app';
-const url = `mongodb+srv://fullstack:${password}@fso-part3-pbook.r9gjw.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+const noteContent = process.argv[2];
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
+
+// Schemas
 const noteSchema = new mongoose.Schema({
   content: String,
   date: Date,
   important: Boolean,
 })
 
+// Model
 const Note = mongoose.model('Note', noteSchema)
 
-const note = new Note({
-  content: 'HTML is Easy',
-  date: new Date(),
-  important: true,
-})
-
-/* note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
-}).catch(err => console.log(err)) */
-
-Note.find({}).then(result => {
-  result.forEach(note => {
-    console.log(note)
+const createNote = () => {
+  const note = new Note({
+    content: noteContent,
+    date: new Date(),
+    important: true,
   })
-  mongoose.connection.close()
-})
+
+  note.save()
+    .then(savedNote => {
+      console.log('note saved:', savedNote)
+      mongoose.connection.close()
+    })
+    .catch(err => console.log(err))
+}
+
+const getNotes = () => {
+  Note.find({}).then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
+    mongoose.connection.close()
+  })
+}
+
+if (process.argv.length === 2) {
+  getNotes();
+}
+
+if (process.argv.length === 3) {
+  createNote();
+}
