4774d4a1a660accec0ba671e1e366fc2e158b754
francoromanolosada <francoromano1994@gmail.com>
2021-03-15 11:07:28 +0100

    4.2 Blog list, step2 exercise.

---
+++ part4/blog-list/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const blogRouter = require('./controllers/blogs')
+
+const app = express()
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
