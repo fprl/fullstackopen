62e0b256472bdf91b41172b9ba18bb3bd3b3600d
francoromanolosada <francoromano1994@gmail.com>
2021-03-17 14:45:37 +0100

    4.17: bloglist expansion, step5 exercise.

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -1,8 +1,13 @@
 const blogRouter = require('express').Router()
+
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({})
+    .populate('user', { username: 1, name: 1, id: 1})
+
   response.json(blogs)
 })
 
@@ -17,16 +22,21 @@ blogRouter.post('/', async (request, response) => {
     response.status(400).end()
     return
   }
-  
+
+  const user = await User.findById(body.userId)
+
   const blog = new Blog({
+    url: body.url,
     title: body.title,
     author: body.author,
-    url: body.url,
-    likes: body.likes || 0
+    user: user._id,
+    likes: body.likes || 0,
   })
 
   const savedBlog = await blog.save()
-  console.log(savedBlog)
+  
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
   
   response.status(201)
   response.json(savedBlog)
--- part4/blog-list/controllers/users.js
+++ part4/blog-list/controllers/users.js
@@ -4,14 +4,14 @@ const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
   const users = await User
-    .find({}).populate('blogs', { content: 1, date: 1 })
+    .find({}).populate('blogs', { url: 1, title: 1, author: 1, id: 1 })
 
   response.json(users)
 })
 
 usersRouter.post('/', async (request, response, next) => {
   const body = request.body
-  
+
   if (body.password.length < 3) {
     return response.status(400).json({
       error: `User validation failed: password: Path 'password' '${body.password}' is shorter than the minimum allowed length (3).`
