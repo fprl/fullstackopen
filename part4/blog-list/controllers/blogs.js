5fdad107182a1b207e8fb1efe6bbbec544ca41f3
francoromanolosada <francoromano1994@gmail.com>
2021-03-17 17:57:30 +0100

    4.19: bloglist expansion, step7 exercise.

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -1,7 +1,16 @@
 const blogRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -23,7 +32,13 @@ blogRouter.post('/', async (request, response) => {
     return
   }
 
-  const user = await User.findById(body.userId)
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     url: body.url,
@@ -34,7 +49,7 @@ blogRouter.post('/', async (request, response) => {
   })
 
   const savedBlog = await blog.save()
-  
+
   user.blogs = user.blogs.concat(savedBlog._id)
   await user.save()
   
