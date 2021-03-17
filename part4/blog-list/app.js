b3b0ec9cf8e3cb44c0c992999da1526284eae6f1
francoromanolosada <francoromano1994@gmail.com>
2021-03-17 13:28:22 +0100

    Following "4.15: bloglist expansion, step3".

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose')
 
 const logger = require('./utils/logger')
 const config = require('./utils/config')
+
 const blogRouter = require('./controllers/blogs')
+const userRouter = require('./controllers/users')
 
 const app = express()
 
@@ -21,6 +23,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/users', userRouter)
 app.use('/api/blogs', blogRouter)
 
 module.exports = app
