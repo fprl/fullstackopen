4a3ce3370dc55bc717d1869911bbe21aaaae429a
francoromanolosada <francoromano1994@gmail.com>
2021-03-22 18:16:15 +0100

    5.18: bloglist end to end testing exercise.

--- part5/blog-list/react-ui/src/components/LoginForm.js
+++ part5/blog-list/react-ui/src/components/LoginForm.js
@@ -29,6 +29,7 @@ export const LoginForm = ({ setUser, handleNotification }) => {
       <div>
         username
         <input
+          id="username"
           type="text"
           value={username}
           name="Username"
@@ -38,13 +39,14 @@ export const LoginForm = ({ setUser, handleNotification }) => {
       <div>
         password
         <input
+          id="password"
           type="password"
           value={password}
           name="Password"
           onChange={({ target }) => setPassword(target.value)}
         />
       </div>
-      <button type="submit">login</button>
+      <button id="login-button" type="submit">login</button>
     </form>
   )
 }
